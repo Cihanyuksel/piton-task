a36fc1671c7318ad45e4f88c303edaff917752c2
Cihan <cihanyyuksel@gmail.com>
2023-06-02 03:49:33 +0300

    deploy bugs fixed

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -4,7 +4,7 @@ export default function Contact() {
   return (
     <div className="h-[500px] flex gap-x-10 md:flex-col md:items-center 3xl:justify-between 3xl:h-[1000px]">
       <div className="w-1/3 h-[500px] md:hidden md:w-1/2 3xl:h-[1000px] relative">
-        <Image src="/images/side-image.png" alt="form image" fill style={{ objectFit: "cover" }}></Image>
+        <Image src="/images/side-image.png" alt="form image" fill style={{ objectFit: "cover" }} />
       </div>
       <div className="w-2/3 h-full flex flex-col items-center justify-between xs:gap-y-2 md:w-full 2xl:gap-y-5 3xl:justify-evenly ">
         <h4 className="w-2/5 text-3xl mb-5 text-end self-end xs:text-xl md:text-center lg:w-full 3xl:text-6xl 3xl:mb-16">
